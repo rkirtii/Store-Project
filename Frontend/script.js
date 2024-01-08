7994b7db99a52ae6ef2aaf1c20d9a8500113975a
rkirtii <142138548+rkirtii@users.noreply.github.com>
2024-01-08 14:09:00 +0530

    Updated script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -38,63 +38,6 @@
 
 
 
-// function fetchData1() {
-//     fetch('http://127.0.0.1:5000/item')
-//         .then(response => response.json())
-//         .then(data => {
-//             displayData(data);
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//             document.getElementById('resultContainer').innerHTML = 'Error fetching data.';
-//         });
-// }
-
-// function fetchData2() {
-//     fetch('http://127.0.0.1:5000/store')
-//         .then(response => response.json())
-//         .then(data => {
-//             displayData(data);
-//         })
-//         .catch(error => {
-//             console.error('Error:', error);
-//             document.getElementById('resultContainer').innerHTML = 'Error fetching data.';
-//         });
-// }
-
-// function displayData(data) {
-//     const resultContainer = document.getElementById('resultContainer');
-//     resultContainer.innerHTML = ''; // Clear previous content
-
-//     const jsonList = document.createElement('ul');
-//     jsonList.classList.add('json-list');
-
-//     for (const key in data) {
-//         const listItem = document.createElement('li');
-//         listItem.classList.add('json-item');
-
-//         const keyElement = document.createElement('span');
-//         keyElement.classList.add('json-key');
-//         keyElement.textContent = key + ':';
-
-//         const valueElement = document.createElement('span');
-//         valueElement.classList.add('json-value');
-//         valueElement.textContent = JSON.stringify(data[key]);
-
-//         listItem.appendChild(keyElement);
-//         listItem.appendChild(valueElement);
-//         jsonList.appendChild(listItem);
-//     }
-
-//     resultContainer.appendChild(jsonList);
-// }
-
-// document.getElementById('fetchDataButton1').addEventListener('click', fetchData1);
-// document.getElementById('fetchDataButton2').addEventListener('click', fetchData2);
-
-
-
-
 
 
 
